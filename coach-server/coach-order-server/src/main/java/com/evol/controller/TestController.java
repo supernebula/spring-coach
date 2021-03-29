9b5b903ab29ddad5328a6e82b39524be2bcec25c
caozhongqiang <caozhongqiang@hzchangchong.com>
2021-03-29 18:48:50 +0800

    logback 日志集成

--- coach-server/coach-order-server/src/main/java/com/evol/controller/TestController.java
+++ coach-server/coach-order-server/src/main/java/com/evol/controller/TestController.java
@@ -1,8 +1,11 @@
 package com.evol.controller;
 
 import com.evol.contant.RabbitContants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
@@ -18,6 +23,14 @@ public class TestController {
     public String testMqUser(String message){
         rabbitTemplate.convertAndSend(RabbitContants.USER_BALANCE_EXCHANGE, RabbitContants.USER_BALANCE_ROUTING_KEY,
                 "message" + message);
+        return message;
+    }
+
+    @GetMapping("/testLog")
+    public String testLog(){
+        String message = "test log" + System.currentTimeMillis();
+        logger.debug(message);
+        logger.info(message);
         return message;
     }
 }

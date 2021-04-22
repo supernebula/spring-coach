194846b52e67ffe4ec1e4928fb59251253a6e69e
supernebula <supernebula@126.com>
2021-04-22 21:25:42 +0800

    Swagger2 ui

--- coach-server/coach-business-server/src/main/java/com/evol/CoachBusinessServerApplication.java
+++ coach-server/coach-business-server/src/main/java/com/evol/CoachBusinessServerApplication.java
@@ -3,15 +3,25 @@ package com.evol;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableEurekaClient
 @SpringBootApplication
+@EnableEurekaClient
+@ConditionalOnClass(CoachBusinessServerApplication.class)
 @MapperScan("com.evol.mapper")
-public class CoachBusinessServerApplication {
+public class CoachBusinessServerApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(CoachBusinessServerApplication.class, args);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
 }

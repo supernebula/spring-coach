3dcd761d1dee2b6050e3d665cfb08c49e1ec4c25
supernebula <supernebula@126.com>
2021-08-31 21:41:17 +0800

    gateway

--- coach-gateway/src/main/java/com/evol/coach/CoachGatewayApplication.java
+++ coach-gateway/src/main/java/com/evol/coach/CoachGatewayApplication.java
@@ -1,25 +1,25 @@
-package com.evol.coach;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-
-
-@SpringBootApplication
-@EnableEurekaClient
-@EnableFeignClients
-public class CoachGatewayApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(CoachGatewayApplication.class, args);
-    }
-
-
-
-}
+//package com.evol.coach;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.gateway.filter.GlobalFilter;
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//
+//
+//@SpringBootApplication
+//@EnableEurekaClient
+//@EnableFeignClients
+//public class CoachGatewayApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(CoachGatewayApplication.class, args);
+//    }
+//
+//
+//
+//}
--- coach-gateway/src/main/java/com/evol/coach/filter/TokenDecoderFilter.java
+++ coach-gateway/src/main/java/com/evol/coach/filter/TokenDecoderFilter.java
@@ -1,32 +1,32 @@
-package com.evol.coach.filter;
-import groovy.util.logging.Slf4j;
-import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.stereotype.Component;
-/**
- * token解析工具类
- */
-@Component
-@Order(-1)
-@Slf4j
-public class TokenDecoderFilter extends AbstractGatewayFilterFactory {
-
-
-    @Override
-    public GatewayFilter apply(Object config) {
-        return (exchange, chain) -> {
-            ServerHttpRequest request = exchange.getRequest();
-            try {
-                Thread.sleep(70);
-            } catch (InterruptedException e) {
-
-                e.printStackTrace();
-            }
-
-            //将现在的request 变成 exchange对象
-            return chain.filter(exchange.mutate().request(request).build());
-        };
-    }
-}
+//package com.evol.coach.filter;
+//import groovy.util.logging.Slf4j;
+//import org.springframework.cloud.gateway.filter.GatewayFilter;
+//import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.http.server.reactive.ServerHttpRequest;
+//import org.springframework.stereotype.Component;
+///**
+// * token解析工具类
+// */
+//@Component
+//@Order(-1)
+//@Slf4j
+//public class TokenDecoderFilter extends AbstractGatewayFilterFactory {
+//
+//
+//    @Override
+//    public GatewayFilter apply(Object config) {
+//        return (exchange, chain) -> {
+//            ServerHttpRequest request = exchange.getRequest();
+//            try {
+//                Thread.sleep(70);
+//            } catch (InterruptedException e) {
+//
+//                e.printStackTrace();
+//            }
+//
+//            //将现在的request 变成 exchange对象
+//            return chain.filter(exchange.mutate().request(request).build());
+//        };
+//    }
+//}

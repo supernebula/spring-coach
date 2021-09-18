d36b2f578d820f5da61234db971b0cae6c53ba39
caozhongqiang <caozhongqiang@hzchangchong.com>
2021-09-18 09:51:22 +0800

    soul 网关 controller 调通

---
+++ coach-soul-gateway/src/main/java/com/evol/CoachSoulGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.evol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachSoulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoachSoulGatewayApplication.class, args);
+    }
+
+}

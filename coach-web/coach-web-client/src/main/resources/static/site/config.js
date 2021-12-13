712cf7f46dfdc21a12baa77dbfc8e72511de05e1
caozhongqiang <caozhongqiang@hzchangchong.com>
2021-12-13 18:05:32 +0800

    web-client

---
+++ coach-web/coach-web-client/src/main/resources/static/site/config.js
@@ -0,0 +1,9 @@
+var apiDomian = "http://localhost:9000";
+
+var config = {
+    authBaseUrl : apiDomian + "/api-auth",
+    businessBaseUrl: apiDomian + "/api-business",
+    logBaseUrl: apiDomian + "/api-log",
+    orderBaseUrl: apiDomian + "/api-order",
+    userBaseUrl: apiDomian + "/api-user"
+}

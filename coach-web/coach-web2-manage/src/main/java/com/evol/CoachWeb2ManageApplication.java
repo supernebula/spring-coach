f862f244f4b64f33f4e65d2f708a0553f2f4a504
caozhongqiang <caozhongqiang@hzchangchong.com>
2022-04-13 17:55:50 +0800

    jwt

---
+++ coach-web/coach-web2-manage/src/main/java/com/evol/CoachWeb2ManageApplication.java
@@ -0,0 +1,13 @@
+package com.evol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachWeb2ManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoachWeb2ManageApplication.class, args);
+    }
+
+}

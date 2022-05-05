44651f68d165cc2a5dd13533ae743320d7b2352e
supernebula <supernebula@126.com>
2022-05-05 14:52:18 +0800

    spring security oauth2

--- coach-web/coach-web-oauth-example/src/main/java/com/evol/config/AuthorizationServerConfig.java
+++ coach-web/coach-web-oauth-example/src/main/java/com/evol/config/AuthorizationServerConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -23,12 +24,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private UserDetailsService userService;
 
+    private TokenStore inMemoryTokenStore;
+
     /**
      * 使用密码模式需要配置
      */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints.authenticationManager(authenticationManager)
+        endpoints.tokenStore(inMemoryTokenStore)
+                .authenticationManager(authenticationManager)
                 .userDetailsService(userService);
     }
 
@@ -37,10 +41,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         clients.inMemory()
                 .withClient("admin")//配置client_id
                 .secret(passwordEncoder.encode("admin123456"))//配置client-secret
+                .authorizedGrantTypes("authorization_code","password")//配置grant_type，表示授权类型
                 .accessTokenValiditySeconds(3600)//配置访问token的有效期
                 .refreshTokenValiditySeconds(864000)//配置刷新token的有效期
                 .redirectUris("http://www.baidu.com")//配置redirect_uri，用于授权成功后跳转
-                .scopes("all")//配置申请的权限范围
-                .authorizedGrantTypes("authorization_code","password");//配置grant_type，表示授权类型
+                .scopes("all");//配置申请的权限范围
+                
     }
 }

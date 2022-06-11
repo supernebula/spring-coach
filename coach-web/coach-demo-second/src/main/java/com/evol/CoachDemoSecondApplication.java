28214d5f8e50c30620076b7e46e8ec8740ce7728
caozhongqiang <caozhongqiang@hzchangchong.com>
2022-06-11 14:27:45 +0800

    none

---
+++ coach-web/coach-demo-second/src/main/java/com/evol/CoachDemoSecondApplication.java
@@ -0,0 +1,25 @@
+package com.evol;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@SpringBootApplication
+public class CoachDemoSecondApplication implements ApplicationRunner {
+    @Autowired
+    private DataSource dataSource;
+    @Override
+    public void run(ApplicationArguments args) throws Exception{
+        try (Connection conn = dataSource.getConnection()){
+            System.out.println(conn);
+        }
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(CoachDemoSecondApplication.class, args);
+    }
+}

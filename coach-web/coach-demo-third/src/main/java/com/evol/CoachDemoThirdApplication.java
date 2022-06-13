73999c5d86122d2104ecc1e40f3220f4a2fd83e2
supernebula <supernebula@126.com>
2022-06-13 13:59:19 +0800

    多数据源

---
+++ coach-web/coach-demo-third/src/main/java/com/evol/CoachDemoThirdApplication.java
@@ -0,0 +1,13 @@
+package com.evol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachDemoThirdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoachDemoThirdApplication.class, args);
+    }
+
+}
